80ef0a0645056d973269f27fa1298fb782d8e0e6
Hai Nguyen <hainm.comp@gmail.com>
2016-06-25 00:31:42 -0400

    add interface

--- mdsrv/mdsrv.py
+++ mdsrv/mdsrv.py
@@ -7,6 +7,8 @@ from __future__ import print_function
 import os
 import sys
 import json
+import array
+import numpy as np
 import logging
 import datetime
 import functools
@@ -296,7 +298,6 @@ class TrajectoryCache(pytraj.TrajectoryIterator):
         return self
 
     def get_frame_string(self, index, **kwargs):
-        import array
 
         frame = self[index]
         return (
@@ -305,6 +306,28 @@ class TrajectoryCache(pytraj.TrajectoryIterator):
             array.array("f", frame.box.to_recip()[0].flatten()).tobytes() +
             array.array("f", frame.xyz.flatten()).tobytes()
         )
+
+    def get_path(self, atom_index, frame_indices=None):
+        if frame_indices is not None:
+            size = len(frame_indices)
+            frame_indices = map(int, frame_indices)
+        else:
+            size = self.n_frames
+            frame_indices  = range(size)
+
+        path = np.zeros((size, 3), dtype=np.float32 )
+
+        for i in frame_indices:
+            frame = self[i]
+            box = frame.box.to_recip()
+            time = frame.time
+            coords = frame.xyz
+            path[i] = coords[atom_index]
+        return path
+
+    def get_path_string(self, atom_index, frame_indices=None):
+        path = self.get_path( atom_index, frame_indices=frame_indices )
+        return array.array( "f", path.flatten() ).tostring()
 
 args = parse_args()
 TRAJ_CACHE = TrajectoryCache(top=args.struc)
@@ -451,10 +474,4 @@ def main():
 
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        try:
-            os.remove('__tmp_pytraj.pdb')
-        except OSError:
-            print("hello")
+    main()
